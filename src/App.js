53f19d0552c146e36899260cefd1627f8e37d51d
itsniranjan <niranjan2neelakantan@gmail.com>
2023-05-12 22:58:06 +0530

    successfully created sha256 hash function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+const crypto = require('crypto');
+
+function hashData(data) {
+  const hash = crypto.createHash('sha256');
+  hash.update(data);
+  return hash.digest('hex');
+}
+
+
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [hashValue, setHashValue] = useState('');
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setInputValue(value);
+    setHashValue(hashData(value));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SHA-256 Hashing</h1>
+      <input
+        type="text"
+        placeholder="Enter a string"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      <p>Hash Value: {hashValue}</p>
     </div>
   );
 }
 
 export default App;
+
